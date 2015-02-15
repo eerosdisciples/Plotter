7d5f6246ab66d84ee497a5532aeee9fd3926ce65
Ingrid <ingstra@student.chalmers.se>
2015-02-15 17:35:45 +0100

    magnetfält från fil

--- readfromfile.py
+++ readfromfile.py
@@ -6,11 +6,18 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # Har delat upp datan i tre filer
 Br = np.genfromtxt('Br')
-print(Br[0,0:5]) # test att det ser ok ut
+#print(Br) # test att det ser ok ut
 
 Bphi = np.genfromtxt('Bphi')
 Bz = np.genfromtxt('Bz')
 
+plt.xlabel('r')
+plt.ylabel('z')
+
+imgplot = plt.imshow(Br, origin='lower')
+plt.show()
+
+  
 
 #fig = plt.figure()
 #ax = fig.add_subplot(111, projection='3d')
