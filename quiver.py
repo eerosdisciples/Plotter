c55be01ad203841443fa4632ba30fdd296c10e00
hoppe93 <mathias.hoppe2060@gmail.com>
2015-02-20 14:10:04 +0100

    Quiver plot now working

--- quiver.py
+++ quiver.py
@@ -4,18 +4,12 @@ import csv
 
 from pylab import *
 
-Br = np.genfromtxt('Br')
-Bz = -np.genfromtxt('Bz')
+Br = np.transpose(np.genfromtxt('Br'))
+Bz = np.transpose(np.genfromtxt('Bz'))
 
 R,Z = meshgrid(linspace(3.5,8.9,257), linspace(-5.5,5.5,513))
 
 figure()
 Q = quiver(R[::12,::12], Z[::12,::12], Br[::12,::12], Bz[::12,::12])
-#quiver(Br[::12,::12],Bz[::12,::12])
-#qk = quiverkey(Q, 0.5, 0.92, 2, r'$2 \frac{m}{s}$', labelpos='W',
-#        fontproperties={'weight':'bold'})
 
 show()
-
-#l,r,b,t = axis()
-#dx,dy = r-l,t-b
