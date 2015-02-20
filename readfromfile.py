1578523a48a00494578897d810add0943252d229
hoppe93 <mathias.hoppe2060@gmail.com>
2015-02-20 13:28:19 +0100

    Changed R and Z in fields

--- readfromfile.py
+++ readfromfile.py
@@ -6,18 +6,19 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # Har delat upp datan i tre filer
 Br = np.genfromtxt('Br')
+Br = map(list, zip(*Br))
 #print(Br) # test att det ser ok ut
 
 Bphi = np.genfromtxt('Bphi')
+Bphi = map(list, zip(*Bphi))
 Bz = np.genfromtxt('Bz')
+Bz = map(list, zip(*Bz))
 
-plt.xlabel('r')
+plt.xlabel('R')
 plt.ylabel('z')
 
-imgplot = plt.imshow(Br, origin='lower')
+imgplot = plt.imshow(Bz, origin='lower')
 plt.show()
-
-  
 
 #fig = plt.figure()
 #ax = fig.add_subplot(111, projection='3d')
