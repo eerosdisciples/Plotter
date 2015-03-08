0c2ee751aafc15a3e1a2adc850519c7832c1baa8
Ingrid <ingstra@student.chalmers.se>
2015-03-08 17:01:36 +0100

    functioning plot of interpolated data

--- quiver_interp.py
+++ quiver_interp.py
@@ -2,17 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 from pylab import *
-Br_short = np.transpose(np.genfromtxt('Output_B_r_short'))
-Bz_short = np.transpose(np.genfromtxt('Output_B_z_short'))
+Br = np.transpose(np.genfromtxt('Output_B_r'))
+Bz = np.transpose(np.genfromtxt('Output_B_z'))
 
-Br = np.transpose(np.genfromtxt('Br'))
-Bz = np.transpose(np.genfromtxt('Bz'))
-
-R,Z = meshgrid(linspace(3.5,8.9,257), linspace(-5.5,5.5,513))
+R,Z = meshgrid(linspace(3.5,8.9,2*257), linspace(-5.5,5.5,2*513))
 figure()
 Q = quiver(R[::12,::12], Z[::12,::12], Br[::12,::12], Bz[::12,::12])
-#show()
+show()
 
-# för testing purposes
-#print("real: ",Br[153,88])
-#print("interp: ", Br_short[153,88])
