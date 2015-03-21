4267f56d6f54a9178377cdfd64c9c16322ece0e0
Ingrid <ingstra@student.chalmers.se>
2015-03-21 17:40:20 +0100

    Plot energy as a function of time

---
+++ energy.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import matplotlib.cbook as cbook
+
+fig = plt.figure()
+
+results = np.genfromtxt('../ode-solver/particle.csv', delimiter=',', skip_header=8)
+
+T = results[:,0]
+E = results[:,7]
+
+plt.plot(T,E, color='r')
+
+plt.xlabel('Time (s)')
+plt.ylabel('Energy (eV)')
+
+plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+#plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
+
+
+plt.grid()
+plt.show()
+
